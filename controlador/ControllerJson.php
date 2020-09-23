1c1c1eab2cbee12fc808891bd8504e110d864f2e
juangarzon616 <jpgarzon947@misena.edu.co>
2020-09-23 13:26:51 -0500

    chore: funciona pero no guarda en la db

--- controlador/ControllerJson.php
+++ controlador/ControllerJson.php
@@ -20,17 +20,17 @@ class ControllerUser{
     public function __construct($id, $tipId, $nam1, $nam2, $nam3, $nam4, $mail, $dir, $tel1, $tel2, $pass){
         $this->id    = $id;
         $this->tipId = $tipId;
-        $this->nam1 = $nam1;
-        $this->nam2 = $nam2;
-        $this->nam3 = $nam3;
-        $this->nam4 = $nam4;
-        $this->mail = $mail;
-        $this->dir  = $dir;
-        $this->tel1 = $tel1;
-        $this->tel2 = $tel2;
-        $this->img  = "../vista/asset/ImgUsers/default.jpg";
-        $this->rol  = 2;
-        $this->pass = $this->hashPass($pass);
+        $this->nam1  = $nam1;
+        $this->nam2  = $nam2;
+        $this->nam3  = $nam3;
+        $this->nam4  = $nam4;
+        $this->mail  = $mail;
+        $this->dir   = $dir;
+        $this->tel1  = $tel1;
+        $this->tel2  = $tel2;
+        $this->img   = "../vista/asset/ImgUsers/default.jpg";
+        $this->rol   = 2;
+        $this->pass  = $this->hashPass($pass);
     }
 
     public function hashPass($pass){
--- modelo/Consultas.php
+++ modelo/Consultas.php
@@ -3,27 +3,27 @@
 require("Conexion.php");
 
 class ConsultasUsuario extends Conexiondb{
-    private $conexion;
-    public function __construct( ){
+    public $conexion;
+    public function __construct(){
         $this->conexion = ConexionDb::Conex();
     }
     public function insertNewUser($DatoUser, $Table){
-        $stmt = $this->conexion->prepare("INSERT INTO $Table(num_usuario, nombre_usuario, segnom_usuario, primer_apellido, segundo_apellido, direccion_usuario, 
+        $stmt = ConexionDb::Conex()->prepare("INSERT INTO $Table(num_usuario, nombre_usuario, segnom_usuario, primer_apellido, segundo_apellido, direccion_usuario, 
         telefono_usuario, segtelefono_usuario, correo_usuario, contraseña_usuario, img_usuario, fk_rolid_rol, fk_tipodocumentoid_documento)
         VALUES (:id,:nam1,:nam2,:nam3,:nam4,:dir,:tel1,:tel2,:mail,:pass,:img,:rol,:tipid')");
-        $stmt->bindParam(':id', $this->id, PDO::PARAM_STR);
-        $stmt->bindParam(':nam1', $DatoUser['nom1'], PDO::PARAM_STR);
-        $stmt->bindParam(':nam2', $DatoUser['nom2'], PDO::PARAM_STR);
-        $stmt->bindParam(':nam3', $DatoUser['nom3'], PDO::PARAM_STR);
-        $stmt->bindParam(':nam4', $DatoUser['nom4'], PDO::PARAM_STR);
-        $stmt->bindParam(':dir', $DatoUser['dir'], PDO::PARAM_STR);
-        $stmt->bindParam(':tel1', $DatoUser['tel1'], PDO::PARAM_STR);
-        $stmt->bindParam(':tel2', $DatoUser['tel2'], PDO::PARAM_STR);
-        $stmt->bindParam(':mail', $DatoUser['mail'], PDO::PARAM_STR);
-        $stmt->bindParam(':pass', $DatoUser['contra'], PDO::PARAM_STR);
-        $stmt->bindParam(':img', "../vista/asset/ImgUsers/default.jpg", PDO::PARAM_STR);
-        $stmt->bindParam(':rol', 3, PDO::PARAM_INT);
-        $stmt->bindParam(':tipid', $DatoUser['tipid'], PDO::PARAM_INT);
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $stmt->bindParam(':nam1', $nam1, PDO::PARAM_STR);
+        $stmt->bindParam(':nam2', $nam2, PDO::PARAM_STR);
+        $stmt->bindParam(':nam3', $nam3, PDO::PARAM_STR);
+        $stmt->bindParam(':nam4', $nam4, PDO::PARAM_STR);
+        $stmt->bindParam(':dir', $dir, PDO::PARAM_STR);
+        $stmt->bindParam(':tel1', $tel1, PDO::PARAM_STR);
+        $stmt->bindParam(':tel2', $tel2, PDO::PARAM_STR);
+        $stmt->bindParam(':mail', $mail, PDO::PARAM_STR);
+        $stmt->bindParam(':pass', $pass, PDO::PARAM_STR);
+        $stmt->bindParam(':img', $img, PDO::PARAM_STR);
+        $stmt->bindParam(':rol', $img, PDO::PARAM_INT);
+        $stmt->bindParam(':tipid', $tipId, PDO::PARAM_INT);
         if($stmt->execute()){
             return true;
         }
