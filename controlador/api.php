0e8b05716542e933b62ed04e0d4aa41cdd6cb332
juangarzon616 <jpgarzon947@misena.edu.co>
2020-09-23 13:41:12 -0500

    chore: cambio los parametros para probar insercion

--- controlador/api.php
+++ controlador/api.php
@@ -7,9 +7,11 @@ function ParametrosValidos($params){
 
 //if(isset($_GET['apicall'])){
     if($_SERVER['REQUEST_METHOD']=='POST'){
-        $PostData = json_decode(file_get_contents("php://input"), true);
+        /*$PostData = json_decode(file_get_contents("php://input"), true);
         
-        $newUser = new ControllerUser($PostData['ide'],$PostData['tipodoc'],$PostData['nom1'],$PostData['nom2'],$PostData['nom3'],$PostData['nom4'],$PostData['mail'],$PostData['dir'],$PostData['tel1'],$PostData['tel2'],$PostData['contra']);
+        $newUser = new ControllerUser($PostData['ide'],$PostData['tipodoc'],$PostData['nom1'],$PostData['nom2'],$PostData['nom3'],$PostData['nom4'],$PostData['mail'],$PostData['dir'],$PostData['tel1'],$PostData['tel2'],$PostData['contra']);*/
+
+        $newUser = new ControllerUser($_POST['identi'],$_POST['tipodoc'],$_POST['nam1'],$_POST['nam2'],$_POST['nam3'],$_POST['nam4'],$_POST['correo'],$_POST['direcc'],$_POST['tele1'],$_POST['tele2'],$_POST['pass1']);
         $Result = $newUser->createUserController($newUser);
         if($Result){
             $Response['error'] = false;
