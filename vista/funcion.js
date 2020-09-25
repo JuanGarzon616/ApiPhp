c91893f216297ec52b5cea0ae670a24f067dd9d6
juangarzon616 <jpgarzon947@misena.edu.co>
2020-09-24 19:13:10 -0500

    chore: se agrega el js

--- vista/funcion.js
+++ vista/funcion.js
@@ -1,5 +1,35 @@
-$(datos());
+//$(datos());
+/*$(function(){}){
+    guardar();
+}*/
 
+function listarDatos(){
+
+    let form = document.forms['newUser'];
+    var datos = {
+        nam1:    form.nam1.value,
+        nam2:    form.nam2.value,
+        nam3:    form.nam3.value,
+        nam4:    form.nam4.value,
+        correo:  form.correo.value,
+        direcc:  form.direcc.value,
+        tele1:   form.tele1.value,
+        tele2:   form.tele2.value,
+        tipodoc: form.tipodoc.value,
+        identi:  form.identi.value,
+        pass1:   form.pass1.value
+    };
+
+    console.log(datos);
+
+    return datos;
+}
+function guardar(){
+    var json = JSON.stringify( listarDatos() );
+    console.log(json);
+}
+
+/*
 function datos(consulta){
     $.ajax({
         url: 'codigos/buscar.php',
@@ -19,9 +49,9 @@ function datos(consulta){
 $(document).on('keyup', '#caja_busqueda', function(){
     var valor = $(this).val();
     if(valor !=" "){
-        buscar_datos(valor);
+        buscarDatos(valor);
     }else{
         buscar_datos();
     }
-});
+});*/
 
