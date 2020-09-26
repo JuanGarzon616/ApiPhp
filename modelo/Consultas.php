3346b03fbfc4b0da6cc933698b8d9d20bc408d79
williamdta <53374719+williamdta@users.noreply.github.com>
2020-09-26 12:57:59 -0500

    nuevas consultas
    se agregaron consultas

--- modelo/Consultas.php
+++ modelo/Consultas.php
@@ -71,8 +71,73 @@ class ConsultasUsuario extends ConexionDb{
         }
         return $usuarios;
 
+    public function updateUsuarioModel($datosModel, $table){
+          $stmt =  $this->conexion->prepare("UPDATE $table set contraseña_usuario =
+          :pass WHERE num_usuario = :id");
 
-  
+        $stmt->bindParam(":pass", $datosModel ["contraseña_usuario"], PDO::PARAM_STR);
+        $stmt->bindParam(":id", $datosModel["id"], PDO::PARAM_INT);
+        if($stmt->execute()){
+            echo "Actualizacion Exitosa";
+        }else{
+            echo"No se pudo hacer la Actualizacion";
+        }
+     }
+     public function deleteUser ($id) {
+        $stmt = ConexionDb::Conex()->prepare("DELETE FROM USUARIO WHERE USUARIO.NUM_USUARIO = :id AND USUARIO.FK_TIPODOCUMENTOID_DOCUMENTO = 1");
+        $stmt->bindParam(':id',    $id,    PDO::PARAM_STR);
+        if($stmt->execute()){
+            echo "funciono eliminar";
+        }
+        else{
+            echo "No funciono eliminar";
+        }
+     
+     public function loginUsuarioModel($datosModel, $table){
+         $stmt =  ConexionDb::Conex()->prepare("SELECT num_usuario, nombre_usuario, segnom_usuario, primer_apellido, segundo_apellido, direccion_usuario, 
+         telefono_usuario, segtelefono_usuario, correo_usuario, contraseña_usuario, img_usuario, fk_rolid_rol, fk_tipodocumentoid_documento, created FROM $table WHERE correo_usuario AND contraseña_usuario = :pass");
+    
+         $stmt->bindParam(":mail", $datosModel[":mail"]);
+         $stmt->bindParam(":pass", $datosModel[":pass"]);
+
+         $stmt->bindParam('num_usuario', $num_usuario);
+         $stmt->bindParam('nombre_usuario',  $nombre_usuario);
+         $stmt->bindParam('segnom_usuario',  $segnom_usuario);
+         $stmt->bindParam('primer_apellido',  $primer_apellido);
+         $stmt->bindParam('segundo_apellido',  $segundo_apellido);
+         $stmt->bindParam('direccion_usuario',  $direccion_usuario);
+         $stmt->bindParam('telefono_usuario',  $telefono_usuario);
+         $stmt->bindParam('segtelefono_usuario',  $segtelefono_usuario);
+         $stmt->bindParam('correo_usuario',  $correo_usuario);
+         $stmt->bindParam('contraseña_usuario',  $contraseña_usuario);
+         $stmt->bindParam('img_usuario',  $img_usuario);
+         $stmt->bindParam('fk_rolid_rol',  $fk_rolid_rol);
+         $stmt->bindParam('fk_tipodocumentoid_documento',  $fk_tipodocumentoid_documento);
+         $stmt->bindParam("created", $created);
+
+         while ($fila = $stmt->fetch(PDO::FETCH_BOUND)){
+
+            $user = array();
+
+            $user["num_usuario"] = utf8_encode($num_usuario);
+            $user["nombre_usuario"] = utf8_encode($nombre_usuario);
+            $user["segnom_usuario"] = utf8_encode($segnom_usuario);
+            $user["primer_apellido"] = utf8_encode($primer_apellido);
+            $user["segundo_apellido"] = utf8_encode($segundo_apellido);
+            $user["telefono_usuario"] = utf8_encode($telefono_usuario);
+            $user["correo_usuario"] = utf8_encode($correo_usuario);
+            $user["contraseña_usuario"] = utf8_encode($contraseña_usuario);
+            $user["img_usuario"] = utf8_encode($img_usuario);
+            $user["fk_rolid_rol"] = utf8_encode($fk_rolid_rol);
+            $user["created"] = utf8_encode($created);
+
+         }
+         if(!empty($user)){
+             return $user;
+         }else{
+             return false;
+         }         
+    }
 }
 
 ?>
