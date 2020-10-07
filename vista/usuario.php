5636bc81a5eb59c0fd86568f5ce585306829cddb
juangarzon616 <jpgarzon947@misena.edu.co>
2020-10-07 16:03:45 -0500

    el william c la come

--- vista/usuario.php
+++ vista/usuario.php
@@ -1,23 +1,59 @@
 <?php
-echo $_COOKIE['firstName'];
+//echo $_COOKIE['firstName'];
 ?>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Cuenta Iniciada</title>
-     <link rel="stylesheet" href="Login-form/css/usuario.css">
-    </head>
-    <body>
-         
-        <div class="box1">
+<head>
+    <meta charset="utf-8">
+    <title>Cuenta Iniciada</title>
+    <link rel="stylesheet" href="Login-form/css/usuario.css">
+    <script src="https://kit.fontawesome.com/5be49774fe.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <nav class="user">
+        <div class="login">
+            <div class="pqr">
+                <h1>PQR´s LAWP</h1>                
+            </div>
+
+            <div class="data">
+                <nav class="imguser">
+                    <img src="asset/imgusers/default.jpg" alt="imgUser">
+                </nav>
+                <nav class="nomb">
+                    <p>Juan Garzón</p>
+                </nav>
+            </div>
+            <div class="tab">
+                <h3><a href=""><i class="far fa-envelope-open"></i> Ver PQR's</a></h3>
+                <h3><a href=""><i class="far fa-envelope"></i> Crear PQR´s</a></h3>
+                <h3><a href=""><i class="far fa-user"></i> Ver Cuenta</a></h3>
+                <h3><a href=""><i class="far fa-user"></i> Ayuda</a></h3>
+            </div>
+        </div>
+        <div class="dash">
+            <div class="cabeza">
+                <h1><a href=""><i class="fas fa-sign-out-alt"></i></a></h1>
+            </div>
+            <div class="masrelleno">
+                <h2><i class="fas fa-hand-sparkles"></i> Bienvenido</h2>
+                <h3><a href=""><i class="fas fa-home"> / Inicio</a></i></h3>
+            </div>
+            <div class="relleno">
+
+            </div>    
+        </div>
+    </nav>
+
+
+
+
+
+
+<!--
+    <div class="box1">
         <img class="avatar" src="Login-form/img/logo.png" alt="logo de Lawp">
-        <h1>USUARIO</h1>
-        <ul>            
-            <li><a href="">Editar Perfil</a></li>
-            <li><a href="index.html">Cerrar Sesion</a></li>
-        </ul>
-             
-    </div>    
         
-    </body>
+    </div>    
+-->        
+</body>
 </html>
